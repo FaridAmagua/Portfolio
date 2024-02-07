ec58cae9f1924758c919c76f5c82f959cda18341
FaridAmagua <faridamagua@live.com>
2024-02-07 17:51:21 +0100

    Home

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, NavbarComponent, HomeComponent]
+})
+export class AppComponent {
+  title = 'Portfolio';
+}
