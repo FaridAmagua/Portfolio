0fd0e5b8f6b802fe4d204f4e3fb3bf45f62b73f3
Farid Amagua <farid.amagua@llorenteycuenca.com>
2024-02-09 12:51:43 +0100

    check animations

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { trigger,state,transition, style, animate } from '@angular/animations';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
+  animations:[
+    trigger('animations',[
+      state('active',style({
+        TransformStream:'translate(10px,20px)'
+      })),
+      state('inactive',style({
+        TransformStream:'translate(40px,50px)'
+      })),
+      transition('inactive => active',animate('1000ms ease-in')),
+      transition('active => inactive',animate('1000ms ease-out'))
+    ])
+  ]
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+state: string = 'inactive'
+  constructor(){}
+  ngOnInit():void{
+    setTimeout({} => {
+      this.state = 'active';
+      },500);
+    }
 }
